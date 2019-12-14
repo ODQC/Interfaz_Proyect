a66c2409eb3e6f0565b286ef469771ef2106bbc2
Oscar Quesada <51036668+ODQC@users.noreply.github.com>
2019-12-14 10:48:46 -0600

    Form Alarmas
    Form de alarmas creado

---
+++ Interfaz/Interfaz/GUI/ConfigAlarm.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Interfaz.GUI
+{
+    public partial class ConfigAlarm : Form
+    {
+        public ConfigAlarm()
+        {
+            InitializeComponent();
+        }
+
+        private void dateTimePicker1_ValueChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void domingoChk_CheckedChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
